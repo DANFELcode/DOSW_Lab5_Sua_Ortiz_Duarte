1028847f36f12c8d175a00f35f3ec0ee9a58e45b
DOrtizSalcedo <david.osalcedo@mail.escuelaing.edu.co>
2026-09-14 21:24:50 -0500

    feature: added the structure for some tests

---
+++ skyrescue-tdd/src/test/java/edu/eci/dosw/tdd/skyrescue/center/RescueCenterTester.java
@@ -0,0 +1,39 @@
+package edu.eci.dosw.tdd.skyrescue.center;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RescueCenterTester {
+    // Casos A
+    @Test
+    void shouldRegisterAnEmptyId() {
+
+    }
+
+    @Test
+    void shouldRegisterTwoDronesWithSameId() {
+
+    }
+
+    // Casos B
+    @Test
+    void shouldNotHaveAnInexistentDrone() {
+
+    }
+
+    @Test
+    void shouldShowADroneIsOccupied() {
+
+    }
+
+    // Casos C
+    @Test
+    void shouldNotCloseTheSameMissionTwoTimes() {
+
+    }
+
+    @Test
+    void shouldNotModifyAnotherActiveMission() {
+
+    }
+}
